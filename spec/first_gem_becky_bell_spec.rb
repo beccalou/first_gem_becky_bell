52bf6156ce668d6b200ff2c1cf4f6dbb9a16838b
Becky Bell <rlbell@alum.bu.edu>
2014-02-12 12:07:51 -0500

    created test for method unique_words

--- spec/first_gem_becky_bell_spec.rb
+++ spec/first_gem_becky_bell_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe String, 'word_count' do
+describe String, '#word_count' do
 	it 'should have a method called word_count' do
 		should respond_to :word_count
 	end
@@ -9,5 +9,13 @@ describe String, 'word_count' do
 		a_string = 'apple'
 		the_word_count = a_string.word_count
 		expect(the_word_count).to eq 1
+	end
+end
+
+describe String, '#unique_words' do
+	it 'should remove duplicate words from string' do
+		repeated_words = 'go go go go'
+		remove_dups = repeated_words.unique_words
+		expect(remove_dups).to eq ['go']
 	end
 end
